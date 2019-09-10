be7367528b9189676d2f577aaaca4b621b4b79de
Annie <annie1.rochette@hotmail.com>
2019-09-10 19:22:45 -0400

    Update Server.java

--- src/Server.java
+++ src/Server.java
@@ -3,12 +3,11 @@ import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.DataOutputStream;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.List;
-import java.util.Stack;
 
 public class Server {
 	private static ServerSocket listener;
@@ -38,4 +37,35 @@ public class Server {
 		
 	}
 	
+
+	private static class ClientHandler extends Thread {
+		private Socket socket;
+		private int clientNumber;
+		
+		public ClientHandler(Socket socket, int clientNumber) {
+			this.socket = socket;
+			this.clientNumber = clientNumber;
+			System.out.println("New connection with client#" + clientNumber + "at" + socket);
+		}
+		
+		public void run() {
+			try {
+				DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+				out.writeUTF("Hello from server - you are client#" + clientNumber);
+				
+			} catch(IOException e) {
+				System.out.println("Error handling client#" + clientNumber + ":" + e);
+			} finally {
+				try {
+					socket.close();
+				}
+				catch(IOException e) {
+					System.out.println("Couldn't close a socket, what's going on ?");
+					
+				}
+				System.out.println("Connection with client#" + clientNumber + "closed");
+			}
+		}
+	}
 }
+
